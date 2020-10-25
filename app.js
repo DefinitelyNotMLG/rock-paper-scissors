11ffe71abb3a8fc0a8e7a670a4264190c43f6473
DefinitelyNotMLG <72935141+DefinitelyNotMLG@users.noreply.github.com>
2020-10-25 17:43:48 -0400

    final push!

--- app.js
+++ app.js
@@ -20,29 +20,37 @@ function convert(letter) {
     return "Scissors";
 }
 
-
 function win(userChoice, computerChoice) {
+    const smallUser = "user".fontsize(3).sup();
+    const smallComp = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore
     compScore_span.innerHTML = compScore
-    const smallUser = "user".fontsize(3).sup();
-    const smallComp = "comp".fontsize(3).sup();
     result_p.innerHTML = `${convert(userChoice)}${(smallUser)} Beats ${convert(computerChoice)}${(smallComp)}, You Win!`;
+    document.getElementById(userChoice).classList.add('green-glow')
+    setTimeout(() => { userChoice_div.classList.remove("green-glow") }, 300);
 }
 
 function loss(userChoice, computerChoice) {
+    const smallUser = "user".fontsize(3).sup();
+    const smallComp = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
     compScore++;
     userScore_span.innerHTML = userScore
     compScore_span.innerHTML = compScore
-    const smallUser = "user".fontsize(3).sup();
-    const smallComp = "comp".fontsize(3).sup();
     result_p.innerHTML = `${convert(userChoice)}${(smallUser)} Loses to ${convert(computerChoice)}${(smallComp)}, You Lost`;
+    document.getElementById(userChoice).classList.add('red-glow')
+    setTimeout(() => { userChoice_div.classList.remove("red-glow") }, 300);
 }
 
 function tie(userChoice, computerChoice) {
     const smallUser = "user".fontsize(3).sup();
     const smallComp = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convert(userChoice)}${(smallUser)} Ties ${convert(computerChoice)}${(smallComp)}, Its a Draw..`;
+    document.getElementById(userChoice).classList.add('grey-glow')
+    setTimeout(() => { userChoice_div.classList.remove("grey-glow") }, 300);
 }
 
 function game(userChoice) {
@@ -67,17 +75,11 @@ function game(userChoice) {
 }
 
 function main() {
-    rock_div.addEventListener('click', function () {
-        game("r");
-    })
+    rock_div.addEventListener('click', () => game("r"));
 
-    paper_div.addEventListener('click', function () {
-        game("p");
-    })
+    paper_div.addEventListener('click', () => game("p"));
 
-    scissors_div.addEventListener('click', function () {
-        game("s");
-    })
+    scissors_div.addEventListener('click', () => game("s"));
 }
 
 main();
